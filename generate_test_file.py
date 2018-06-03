1319a997906fb8ddc0be9af9df71be8290f5a8b5
testTaskKisialeu <39881224+testTaskKisialeu@users.noreply.github.com>
2018-06-03 21:48:00 +0200

    add file to generate test file contains rand text

---
+++ generate_test_file.py
@@ -0,0 +1,21 @@
+import random
+import string 
+
+
+def rand_str(size=6, chars=string.ascii_lowercase + string.ascii_lowercase ):
+	c = 0
+	a = []
+	while c < 10:
+		a.append(''.join(random.choice(chars) for _ in range(size)))
+		c += 1
+		if c//5 == 0:
+			a.append(random.randint(1,999))
+	
+	return str(a)
+
+
+c =0
+with open(r'softeq_test', 'a') as log_file:
+	while c < 20:
+		log_file.write(rand_str() + rand_str() +'\n')
+		c +=1
