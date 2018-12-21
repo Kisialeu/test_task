bd119467725e2f96697a7115a197ba39b9e6782c
Kisialeu <kisialeu@gmail.com>
2018-12-21 17:41:41 +0300

    Update test_worker.py

--- test_worker.py
+++ test_worker.py
@@ -277,3 +277,19 @@ if __name__ == '__main__':
             failfast=False,
             buffer=False,
             catchbreak=False)
+import json
+import xmltodict
+ 
+with open("results.xml", 'r') as f:
+    xmlString = f.read()
+ 
+print("XML input (sample.xml):")
+print(xmlString)
+
+jsonString = json.dumps(xmltodict.parse(xmlString), indent=4)
+ 
+print("\nJSON output(output.json):")
+print(jsonString)
+ 
+with open("output.json", 'w') as f:
+    f.write(jsonString)
